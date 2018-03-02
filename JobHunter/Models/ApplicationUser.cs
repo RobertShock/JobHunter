1dcc7c52a967b7470f7803d26ac2e09f116e20a3
Robbie Shock <rob.f.shock@gmail.com>
2018-03-02 08:06:02 -0800

    initial setup of models

---
+++ JobHunter/Models/ApplicationUser.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace JobHunter.Models
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        [Display(Name = "First Name")]
+        [StringLength(55, ErrorMessage = "Sorry, that's too long. Please enter your nickname")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        [StringLength(55, ErrorMessage = "Sorry, that's too long")]
+        public string LastName { get; set; }
+
+        public virtual ICollection<Job> SavedJobs { get; set; }
+
+        public virtual ICollection<Job> AppliedJobs { get; set; }
+    }
+}
